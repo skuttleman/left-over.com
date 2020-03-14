1905bbeec796bc9ad0cd4edcdb6278742f365131
Ben Allred <skuttleman@gmail.com>
2020-03-14 10:59:26 -0600

    fix heroku deploy script

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -2,8 +2,16 @@
 
 set -e
 
-APP=${1:left-over-api}
+APP=${1:-left-over-api}
 
-lein do clean, uberjar
+lein clean
+rm -rf target
+mkdir target
+mv .lein-env .lein-env.dev
+cp .lein-env.prod .lein-env
+
+lein uberjar
 heroku deploy:jar target/website-0.1.0-SNAPSHOT-standalone.jar --app ${APP}
 heroku run --app ${APP} -- java -cp $(ls target/*-standalone.jar) com.left_over.api.services.db.migrations migrate
+
+mv .lein-env.dev .lein-env
