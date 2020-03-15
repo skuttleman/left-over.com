716d7102468f7262532c1d66e58037d9ae05e332
Ben Allred <skuttleman@gmail.com>
2020-03-14 22:21:01 -0600

    fix heroku deploy script

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -4,6 +4,6 @@ set -e
 
 APP=${1:-left-over-api}
 
-lein do, clean uberjar
+lein do clean, uberjar
 heroku deploy:jar target/website-0.1.0-SNAPSHOT-standalone.jar --app ${APP}
 heroku run --app ${APP} -- java -cp $(ls target/*-standalone.jar) com.left_over.api.services.db.migrations migrate
