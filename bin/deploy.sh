afbcfb43900117b97418daf88a2558f7bb9d0b29
Ben Allred <skuttleman@gmail.com>
2020-03-17 16:31:04 -0600

    remove heroku deploy script

--- bin/deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-APP=${1:-left-over-api}
-
-lein do clean, uberjar
-heroku deploy:jar target/website-0.1.0-SNAPSHOT-standalone.jar --app ${APP}
-heroku run --app ${APP} -- java -cp $(ls target/*-standalone.jar) com.left_over.api.services.db.migrations migrate
